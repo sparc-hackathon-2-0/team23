f4593f68738a23c752d8e062f9d6127eeb3480d1
Michael Feliciano <mefelici@g.cofc.edu>
2012-08-25 20:08:00 -0400

    Finito!

--- gen/com/example/whatiscommunityservice/R.java
+++ gen/com/example/whatiscommunityservice/R.java
@@ -19,20 +19,21 @@ public final class R {
         public static final int match=0x7f020005;
     }
     public static final class id {
-        public static final int bCompleteForm=0x7f070023;
+        public static final int bCompleteForm=0x7f070024;
         public static final int bGenerate=0x7f070014;
-        public static final int bGenerator=0x7f07001e;
-        public static final int bLocal=0x7f07001d;
+        public static final int bGenerator=0x7f07001d;
+        public static final int bLocal=0x7f070022;
         public static final int bLook=0x7f070018;
-        public static final int bMatching=0x7f070020;
+        public static final int bMatching=0x7f070023;
         public static final int bStart=0x7f070002;
+        public static final int bTrack=0x7f070021;
         public static final int button1=0x7f070016;
-        public static final int button3=0x7f07001f;
+        public static final int button3=0x7f07001e;
         public static final int etCity=0x7f070017;
-        public static final int imageView1=0x7f070001;
-        public static final int imageView2=0x7f070022;
-        public static final int imageView3=0x7f070021;
-        public static final int menu_settings=0x7f070024;
+        public static final int imageView1=0x7f070003;
+        public static final int imageView2=0x7f070020;
+        public static final int imageView3=0x7f07001f;
+        public static final int menu_settings=0x7f070025;
         public static final int radio0=0x7f070005;
         public static final int radio1=0x7f070006;
         public static final int radio10=0x7f070012;
@@ -52,8 +53,8 @@ public final class R {
         public static final int tab2=0x7f07001b;
         public static final int tab3=0x7f07001c;
         public static final int tabhost=0x7f070019;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070003;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070000;
         public static final int textView3=0x7f07000e;
         public static final int tvidea=0x7f070015;
     }
